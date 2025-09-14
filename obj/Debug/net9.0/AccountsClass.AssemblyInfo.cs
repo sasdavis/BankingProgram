f35440696bd3d2bc932e54dd052b5ef9af2e5c9c
Sas Davis <sjdav@hotmail.com>
2025-09-14 13:01:58 +1000

    added gitignore

--- obj/Debug/net9.0/AccountsClass.AssemblyInfo.cs
+++ obj/Debug/net9.0/AccountsClass.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccountsClass")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d6e274109f7caafbc15bfbb9c57d821e936a4fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccountsClass")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccountsClass")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
